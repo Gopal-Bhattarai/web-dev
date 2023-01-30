11729e55d83351465d2196024cead14c153adffb
Gopal-Bhattarai <gbhattarai55@gmail.com>
2023-01-30 20:08:31 +0545

    OwnerDetail added

--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,18 +1,47 @@
-import { Box, Paper, Typography } from "@mui/material"
+import { Box, Button, Paper, Stack, TextField, Typography } from "@mui/material"
+import { ContactDetail } from "./OwnerDetail"
+
+const handleSubmit = () => {
+
+}
 
 const ContactInfo = () => {
   return (
-    <Paper sx={{px:3, py:5}}>
-		
-		<Box>
-			<Box>
-				<h3>Visit Our Offices</h3>
-				<Typography>Head Office: Dhumbarahi-4, Pipalbot, Kathmandu</Typography>
-				<Typography>Branch Office: Chapagaundobato-Satdobato, Lalitpur</Typography>
-				<Typography> 9843891333</Typography>
-			</Box>
+	<form onSubmit={handleSubmit}>
+		<Box mt={8}  width="1200px">
+			<Stack direction='row' justifyContent='space-between' my={2} p={2}>
+
+				<Paper elevation={5}>
+					<Box width="200px" height="100px" border={1} borderRadius={2} padding={2}>
+							<Typography variant='overline'>Address</Typography>
+							<Typography variant='body2'>{ContactDetail.address}</Typography>	
+					</Box>
+				</Paper>
+
+				<Paper elevation={5}>
+					<Box width="200px" height="100px" border={1} borderRadius={1} padding={2}>
+							<Typography variant='overline'>Email</Typography>
+							<Typography variant='body2'>{ContactDetail.email}</Typography>	
+					</Box>
+				</Paper>
+
+				<Paper elevation={5}>
+					<Box width="200px" height="100px" border={1} borderRadius={2} padding={2}>
+							<Typography variant='overline'>Contact</Typography>
+							<Typography variant='body2'>{ContactDetail.phone}</Typography>	
+					</Box>
+				</Paper>
+			</Stack>
+
+			<Paper sx={{p:5}}>
+				<Typography variant='h4' sx={{mb:2}}>Get in Touch</Typography>
+				<TextField label="Full Name" fullWidth autocomplete="none"/>
+				<TextField sx={{my:3}} label="Email" fullWidth autocomplete="none"/>
+				<TextField label="Message" fullWidth multiline rows={5} autocomplete="none"/>
+				<Button sx={{mt:5}} type="submit" variant="contained" fullWidth>send</Button>
+			</Paper>
 		</Box>
-    </Paper>
+	</form>
   )
 }
 
