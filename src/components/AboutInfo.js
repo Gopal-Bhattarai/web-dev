aad5e9a45c83e941ab4395d67da513852c5ebbdf
Gopal-Bhattarai <gbhattarai55@gmail.com>
2023-01-31 05:52:47 +0545

    about page

--- src/components/AboutInfo.js
+++ src/components/AboutInfo.js
@@ -1,20 +1,19 @@
-import { Box, Paper, Typography } from "@mui/material"
-import Image from "next/image"
+import { Box, Paper, Stack, Typography } from "@mui/material"
+import {Name, Motto} from './OwnerDetail'
 
 const AboutInfo = () => {
   return (
-    <Paper sx={{p:3}}>
-      <Typography variant="h1" gutterBottom>About Nepal Sami </Typography>
+    <Stack sx={{p:3}}>
+      <Typography variant="h3" gutterBottom>About {Name} </Typography>
       <Box display="fles" justifyContent="center" alignItems="center">
 
-      <Image src = '/img/doctorwithproducts.jpg' alt='products' width={600} height={400} />
       </Box>
-      <Typography variant="h3" sx={{m:2}}>Health, Education & Prosperity</Typography>
+      <Typography variant="h4" sx={{m:2, color: 'navy'}}>{Motto}</Typography>
         <Typography  sx={{m:2}}>Committed for definite health, prosperity and knowledge. 
             Our first mission is to provide you clinically relevant nutraceuticals and cosmaceuticals for well being and being young,</Typography> 
         <Typography  sx={{m:2, pb:20}}>Secondly to give you low cost opportunity to become successful self employed and finally to teach, train & motivate you to build your business for your definite success..</Typography>
 
-    </Paper>
+    </Stack>
   )
 }
 
--- src/components/OwnerDetail.js
+++ src/components/OwnerDetail.js
@@ -3,6 +3,7 @@ export const Name = 'GB Technology P. Ltd'
 export const Title = 'GB Technology'
 export const MetaTag = 'Technology in Nepal'
 export const KeyWords = 'Technology in Nepal via GB Technology'
+export const Motto = 'Delivery of Machines'
 
 export const WhiteLogo = '/img/GB_Tech_Logo.png'
 export const BlackLogo = '/img/logo_dark.png'
